ad498778008f776ff443b302379891abafb7fdaf
Felipe da Silva Stella <luis.felipe@unidasul.com.br>
2022-05-18 15:53:45 -0300

    add project

---
+++ FelipeAguiar_POO/src/Entities/Wizard.cs
@@ -0,0 +1,12 @@
+namespace FelipeAguiar_POO.src.Entities
+{
+  public class Wizard : Hero
+  {
+    public Wizard(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+    {
+    }
+    public override string Attack(){
+        return $"{this.Name} lançou magia";
+    }
+  }
+}
